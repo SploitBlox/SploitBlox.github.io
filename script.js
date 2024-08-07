bf8d1385874c53e96aad22c81f137241ba723251
SploitBlox <spacecreds@hotmail.com>
2024-08-07 16:01:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const progressBars = document.querySelectorAll('.progress');
+    
+    progressBars.forEach(bar => {
+        const width = bar.style.width;
+        bar.style.width = '0%';
+        setTimeout(() => {
+            bar.style.width = width;
+        }, 500);
+    });
+});
